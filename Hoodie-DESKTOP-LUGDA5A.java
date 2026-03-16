f0faa1b2c4b3712c7e4ed6eacb79a9a47f433193
lohith861 <lohithsamrat8618@gmail.com>
2026-03-16 11:25:34 +0530

    variables

---
+++ Hoodie-DESKTOP-LUGDA5A.java
@@ -0,0 +1,26 @@
+class Hoodie {
+    public static void main(String[] args) {
+
+        String materialComposition = "Microfiber";
+        String style = "Modern";
+        String fitType = "Loose";
+        String length = "Standard Length";
+        String careInstruction = "Hand Wash Only";
+        String countryOfOrigin = "India";
+        String department = "Men";
+        String manufacturer = "Bangalore";
+        String genericName = "Hoodie";
+        int price = 1729;
+
+        System.out.println("The hoodie material is: " + materialComposition);
+        System.out.println("The hoodie style is: " + style);
+        System.out.println("The hoodie fit type is: " + fitType);
+        System.out.println("The hoodie length is: " + length);
+        System.out.println("Care instructions: " + careInstruction);
+        System.out.println("Country of origin: " + countryOfOrigin);
+        System.out.println("Department: " + department);
+        System.out.println("Manufacturer: " + manufacturer);
+        System.out.println("Product name: " + genericName);
+        System.out.println("Price: ₹" + price);
+    }
+}
---
+++ UserProfile-DESKTOP-LUGDA5A.java
@@ -0,0 +1,22 @@
+class UserProfile {
+
+    // first name, last name, mobile number, email, date of birth, password, gender
+
+    public static void main(String[] anything) {
+
+        // User profile details
+        String firstName = "Baba";
+        String gender = "Male";
+        long mobileNumber = 8618487480L;
+        String emailId = "lohithsamrat8618@gmail.com";
+        String password = "lohi123";
+        String dateOfBirth = "08/02/2003";
+
+        System.out.println("The user first name is: " + firstName);
+        System.out.println("The user gender is: " + gender);
+        System.out.println("The user mobile number is: " + mobileNumber);
+        System.out.println("The user emailId is: " + emailId);
+        System.out.println("The user password is: " + password);
+        System.out.println("The user date of birth is: " + dateOfBirth);
+    }
+}
